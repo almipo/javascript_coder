5ab1a802cf764447affc357e550db247b8fd21ee
Alan Pozzi <alanpozzi@gmail.com>
2022-10-11 12:54:25 -0300

    agreagdo de funcion para el descuento

--- script.js
+++ script.js
@@ -6,15 +6,19 @@
 
 console.log("BIENVENIDO A MAYORISTA MAYOFRUIT");
 
+//funcion de bienvenida
 
 function saludar_usuario(){
+
+
     let cliente=prompt("ingrese su usuario");   
     console.log("BIENVENIDO" , cliente);
 }
 saludar_usuario();
 
+//lista de productos para pedir
 console.log("nuestros productos en stock son");
-console.log("vVERDURAS")
+console.log("VERDURAS")
 console.log("cebolla: $500");
 console.log("papa: $200");
 console.log("boniato: $200");
@@ -49,6 +53,7 @@ const precio_perejil=500;
 const precio_alabahaca=700;
 
 
+//variables de los productos
 let producto;
 let cantidad;
 let total_cebolla=0;
@@ -69,6 +74,7 @@ let total_hierba=0;
 let suma=0;
 let total=0;
 let medio_de_pago;
+let descuento;
 
 
 console.log("su pedido es el siguiente:");
@@ -102,8 +108,12 @@ while(producto != "total verdura" ){
         total_tomate = (precio_tomate * cantidad)+total_tomate;
         console.log("valor: $" , total_tomate);
     }
+    else if(producto = "total verdura"){
+        console.log("el total a abonar de verdura es: $" , total_verdura );
 
-    else {
+    }
+
+    else{
         console.log("no tenemos esa verdura, ingrese otra")
         }
 
@@ -111,7 +121,6 @@ while(producto != "total verdura" ){
 
 }
 
-console.log("el total a abonar de verdura es: $" , total_verdura )
 
 //ingreso de frutas al pediod
 while(producto != "total fruta"){
@@ -120,35 +129,39 @@ while(producto != "total fruta"){
 
     if (producto == "manzana" || producto == "MANZANA"){
         console.log("manzana: ", cantidad , ("kg"));
-        total_manzana =( precio_manzana * cantidad)+precio_manzana;
+        total_manzana =( precio_manzana * cantidad)+total_manzana;
         console.log("valor: $" , total_manzana );
 
     }
     
     else if(producto == "pera" || producto=="PERA"){
         console.log("pera: ", cantidad , ("kg"));
-        total_pera = (precio_pera * cantidad)+precio_pera;
+        total_pera = (precio_pera * cantidad)+total_pera;
         console.log("valor: $" , total_pera);
     }
     else if(producto == "banana"||producto=="BANANA"){
         console.log("banana: ", cantidad , ("kg"));
-        total_banana = (precio_banana * cantidad)+precio_banana;
+        total_banana = (precio_banana * cantidad)+total_banana;
         console.log("valor: $" , total_banana);
     }
     else if(producto == "mango"||producto=="MANGO"){
         console.log("mango: ", cantidad , ("kg"));
-        total_mango = (precio_mango * cantidad)+precio_mango;
+        total_mango = (precio_mango * cantidad)+total_mango;
         console.log("valor: $" , total_mango);
     }
 
+    else if(producto = "total fruta"){
+        console.log("el total a abonar de fruta es: $" , total_fruta);        
+
+    }
+
     else {
-        console.log("no tenemos esa fruta, ingrese otra")
+        console.log("no tenemos esa fruta, ingrese otra");
         }
 
-    total_fruta = total_manzana + total_pera + total_banana + total_mango;
 
 }
-console.log("el total a abonar de fruta es: $" , total_fruta);
+total_fruta = total_manzana + total_pera + total_banana + total_mango;
 
 
 //ingreso de hierbas al pedido
@@ -158,26 +171,31 @@ while(producto != "total hierbas"){
 
     if (producto == "romero" || producto == "ROMERO"){
         console.log("romero: ", cantidad , ("kg"));
-        total_romero = (precio_romero * cantidad)+precio_romero;
+        total_romero = (precio_romero * cantidad)+total_romero;
         console.log("valor: $" , total_romero);
     }
     
     else if(producto == "cilantro" || producto=="CILANTRO"){
         console.log("cilantro: ", cantidad , ("kg"));
-        total_cilantro = (precio_cilantro * cantidad)+precio_cilantro;
+        total_cilantro = (precio_cilantro * cantidad)+total_cilantro;
         console.log("valor: $" , total_cilantro );
     }
 
     else if(producto == "perejil"||producto=="PEREJIL"){
         console.log("perejil: ", cantidad , ("kg"));
-        total_perejil = (precio_perejil * cantidad)+precio_perejil;
+        total_perejil = (precio_perejil * cantidad)+total_perejil;
         console.log("valor: $" , total_perejil);
     }
 
     else if(producto == "albahaca"||producto=="ALBAHACA"){
         console.log("albahaca: ", cantidad , ("kg"));
-        total_albahaca = (precio_albahaca * cantidad)+precio_albahaca;
+        total_albahaca = (precio_albahaca * cantidad)+total_albahaca;
         console.log("valor: $" , total_albahaca);
+    }
+    else if(producto="total hierbas"){
+        console.log("el total a abonar de hierbas es: $" , total_hierba); 
+
+
     }
 
     else {
@@ -188,7 +206,8 @@ while(producto != "total hierbas"){
 
 }
 
-console.log("el total a abonar de fruta es: $" , total_hierba );
+
+
 
 //valor de la variable suma
 suma= total_verdura + total_fruta + total_hierba;
@@ -203,7 +222,33 @@ else{
 }
 
 
-//total de toda la compra
+
+//total pedido
 console.log("el total de su pedido a abonar es: $" , total);
 
-saludar_usuario();
+//calcular si tiene descuento
+function calcular_descuento(total){
+
+    if(total>= 10000){
+  descuento = total * 0.1;
+    return descuento;
+    
+}
+else{
+    descuento=0
+}
+}
+
+
+//total con descuento
+
+let total_con_descuento = total - calcular_descuento(total);
+
+if(descuento > 0){
+console.log("por superar los $10000, tiene un descuento del 10%, su total seria: $", total_con_descuento);
+}
+else{
+    console.log("usted no supera los $10000,con su compra mayor a $10000 optinene descuento del 10%");
+}
+
+
