d176be13fca39628588424709347d985454083aa
Alan Pozzi <alanpozzi@gmail.com>
2022-10-18 01:35:17 -0300

    agregado de funciones

--- script.js
+++ script.js
@@ -20,13 +20,20 @@ let local_uno = {
 let local_dos = {
     direccion : "cabildo 500",
     localidad: "capital federa", 
-    telefono:5555-555,
+    telefono:5555555,
     cp:2415,
     horario:"10hs a 24hs"
 };
 
-console.log("puede encontrarnos en nuestro local de provincia de bsas", local_uno);
-console.log("tambien puede encontrarnos en capital federal", local_dos);
+console.log("puede encontrarnos en nuestro local de provincia de bsas ubicaso en", local_uno.direccion, local_uno.localidad);
+console.log("estamos abiertos de lunes a viernes", local_uno.horario);
+console.log("o puede contactarnos al telefono:", local_uno.telefono)
+
+console.log("si estas en capital federal....")
+console.log("puede encontrarnos en nuestro local de capital federal ubicado en", local_dos.direccion, local_dos.localidad);
+console.log("estamos abiertos de lunes a viernes", local_dos.horario);
+console.log("o puede contactarnos al telefono:", local_dos.telefono)
+
 
 //uso de objeto para agregar datos del usuario
 class usuario{
@@ -104,9 +111,10 @@ let total_verdura=0;
 let total_fruta=0;
 let total_hierba=0;
 let suma=0;
-let total=0;
+let total;
 let medio_de_pago;
-let descuento;
+let descuento=0;
+let cuenta_final=0;
 
 
 console.log("su pedido es el siguiente:");
@@ -190,9 +198,8 @@ while(producto != "total fruta"){
         console.log("no tenemos esa fruta, ingrese otra");
         }
 
-}
 total_fruta = total_manzana + total_pera + total_banana + total_mango;
-
+}
 
 //ingreso de hierbas al pedido
 while(producto != "total hierbas"){
@@ -238,22 +245,24 @@ while(producto != "total hierbas"){
 
 
 //valor de la variable suma
-function total_sumatoria(suma){
 
     suma= total_verdura + total_fruta + total_hierba;
-    return suma
-}
+    console.log("la suma de los productos es: $", suma);
+
 
 //calculo de medio de pago
-medio_de_pago=prompt("quiere pagar con: \n efectivo \n tarjeta");
-if(medio_de_pago == "tarjeta"){
-    total= total_sumatoria * 1.15;
-}
-else{
-    total=suma;
-}
+function formas_de_pago(medio_de_pago){
 
-
+    if(medio_de_pago == "tarjeta"){
+        total=suma * 1.15;
+        console.log("el monto tiene un recargo del 15% por pago con tarjeta");
+    }
+    else{
+        total=suma;
+    }
+}
+medio_de_pago=prompt("quiere pagar con: \n efectivo \n tarjeta(recargo del 15%)");
+formas_de_pago(medio_de_pago);
 
 //total pedido
 console.log("el total de su pedido a abonar es: $" , total);
@@ -262,25 +271,27 @@ console.log("el total de su pedido a abonar es: $" , total);
 function calcular_descuento(total){
     if(total>= 10000){
         descuento = total * 0.1;
-        return descuento;
+        
     }
     else{
         descuento=0
     }
+    return descuento
 }
+calcular_descuento(total);
 
 
 //total con descuento
 
 
-function total_con_descuento(){
-    let total_con_descuento = total - calcular_descuento(total);
+function total_a_pagar(total, descuento){
     if(descuento > 0){
-        console.log("por superar los $10000, tiene un descuento del 10%, su total seria: $", total_con_descuento);
+        cuenta_final = total - descuento;
+        console.log("por superar los $10000, tiene un descuento del 10%, su total seria: $", cuenta_final);    
     }
     else{
-        console.log("usted no supera los $10000,con su compra mayor a $10000 optinene descuento del 10%");
+        console.log("usted no supera los $10000, si su compra es mayor a $10000 optinene descuento del 10%, el monto queda en:", total);
     }
 }
-
+total_a_pagar(total, descuento);
 
